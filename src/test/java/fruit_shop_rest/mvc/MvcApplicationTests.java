22c78c0a1e8ab839ecddb4740e8fc46d3da7a32b
farzad <farzadnoruzi1900@gmail.com>
2021-09-06 18:21:04 +0200

    starting the project.

---
+++ src/test/java/fruit_shop_rest/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fruit_shop_rest.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
